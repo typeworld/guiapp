6d085cbb356394f7126514807fcca2a3eb9d927a
Yanone <post@yanone.de>
2018-02-04 14:40:17 +0200

    Updates localization

--- wxPython/updateLocalization.py
+++ wxPython/updateLocalization.py
@@ -4,7 +4,9 @@ import os
 from ynlib.web import GetHTTP
 from ynlib.files import WriteToFile
 
-json = GetHTTP('https://type.world/type.world/downloadLocalization?appID=world.type.guiapp')
+json = GetHTTP('https://type.world/downloadLocalization?appID=world.type.guiapp')
 WriteToFile(os.path.join(os.path.dirname(__file__), 'locales', 'content.json'), json)
+
+print json
 
 print 'Updated localization'

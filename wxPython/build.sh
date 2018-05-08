c90d9d5a51a57bdc13ee9f2748bf22fa2163e73b
Yanone <post@yanone.de>
2018-05-08 14:14:56 +0300

    New Sparkle Version

--- wxPython/build.sh
+++ wxPython/build.sh
@@ -5,13 +5,13 @@ rm -rf ~/Code/TypeWorldApp/dist
 
 
 # Build
-python setup.py py2app
+python3 setup.py py2app
 
 # Copy Sparkle over
-cp -R ~/Code/Sparkle/Sparkle.framework ~/Code/TypeWorldApp/dist/Type.World.app/Contents/Frameworks/
+cp -R ~/Code/Sparkle-1.19.0/Sparkle.framework ~/Code/TypeWorldApp/dist/Type.World.app/Contents/Frameworks/
 
 # Copy docktileplugin
-cp -R /Users/yanone/Code/py/git/typeWorld/guiapp/appbadge/dist/appbadge.docktileplugin ~/Code/TypeWorldApp/dist/Type.World.app/Contents/Resources/
+# cp -R /Users/yanone/Code/py/git/typeWorld/guiapp/appbadge/dist/appbadge.docktileplugin ~/Code/TypeWorldApp/dist/Type.World.app/Contents/Resources/
 
 # Move app to archive folder
 cp -R ~/Code/TypeWorldApp/dist/Type.World.app ~/Code/TypeWorldApp/apps/Type.World.`cat ~/Code/TypeWorldApp/build/version`.app

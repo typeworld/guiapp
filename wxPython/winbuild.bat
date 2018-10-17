1b1d7bf652b7ae29fc47b7aae27af97ddaa6b706
Yanone <post@yanone.de>
2018-10-17 19:49:13 +0300

    Update wxPython/winbuild.bat

--- wxPython/winbuild.bat
+++ wxPython/winbuild.bat
@@ -4,3 +4,4 @@ python Z:\Code\py\git\typeWorld\guiapp\wxPython\setup_windows.py build
 "Z:\Code\Windows\Dev Tools\mt.exe" -manifest Z:\Code\py\git\typeWorld\guiapp\wxPython\windowsAppManifest.xml -outputresource:Z:\Code\TypeWorldApp\Windows\Main\TypeWorld.exe;#1
 mkdir "Z:\Code\TypeWorldApp\Windows\Main\URL Opening Agent"
 xcopy "Z:\Code\TypeWorldApp\Windows\Agent" "Z:\Code\TypeWorldApp\Windows\Main\URL Opening Agent" /s /e /y
+Z:\Code\TypeWorldApp\Windows\Main\TypeWorld.exe

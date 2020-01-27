a869101336f92ea71fa1c314e8900669c2ed8b95
Yanone <post@yanone.de>
2020-01-27 18:22:43 +0100

    Create uploadLocalization.py

---
+++ wxPython/uploadLocalization.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+## This file parses all localized keywords from a bunch of relevant files and 
+
+import os, re, keyring, urllib, urllib.request, certifi, ssl
+sslcontext = ssl.create_default_context(cafile=certifi.where())
+#MOTHERSHIP = 'https://typeworld2.appspot.com'
+MOTHERSHIP = 'http://127.0.0.1:8080'
+
+keywords = []
+
+for path in (
+		'htmlfiles/main/index.html',
+		'app.py',
+		'/Users/yanone/Code/py/git/typeworld/typeworld/Lib/typeWorld/client/__init__.py',
+		'/Users/yanone/Code/py/git/typeworld/typeworld/Lib/typeWorld/test.py',
+	):
+	
+	if not path.startswith('/'):
+		path = os.path.join(os.path.dirname(__file__), path)
+
+	f = open(path, "r")
+	content = f.read()
+	f.close()
+
+	for keyword in re.findall(r'#\((.+?)\)', content):
+		if not keyword in keywords \
+			and not '%s' in keyword \
+			and not "'" in keyword \
+			and not ('response.' in keyword and ' ' in keyword) \
+			:
+			keywords.append(keyword)
+
+			if ',' in keyword:
+				print('ALARM ALARM ALARM ALARM: Comma in %s' % keyword)
+
+if not 'TRAVIS' in os.environ: authKey = keyring.get_password(MOTHERSHIP, 'revokeAppInstance')
+else: authKey = os.environ['REVOKEAPPINSTANCEAUTHKEY']
+
+# Announce subscription update
+url = MOTHERSHIP + '/uploadTranslationKeywords'
+parameters = {"authorizationKey": authKey,
+			"keywords": ','.join(keywords),
+			}
+data = urllib.parse.urlencode(parameters).encode('ascii')
+response = urllib.request.urlopen(url, data, context=sslcontext)
+print(response)

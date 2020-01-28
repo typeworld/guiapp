450e9b84006803599e2c68e6dcad46d3cb0df4e1
Yanone <post@yanone.de>
2020-01-28 14:34:21 +0100

    Parse all relevant files for translation keywords #(...) and upload them to the translations DB

--- wxPython/uploadLocalization.py
+++ wxPython/uploadLocalization.py
@@ -4,8 +4,8 @@
 
 import os, re, keyring, urllib, urllib.request, certifi, ssl
 sslcontext = ssl.create_default_context(cafile=certifi.where())
-#MOTHERSHIP = 'https://typeworld2.appspot.com'
-MOTHERSHIP = 'http://127.0.0.1:8080'
+MOTHERSHIP = 'https://typeworld2.appspot.com'
+#MOTHERSHIP = 'http://127.0.0.1:8080'
 
 keywords = []
 
@@ -19,9 +19,17 @@ for path in (
 	if not path.startswith('/'):
 		path = os.path.join(os.path.dirname(__file__), path)
 
+	content = []
 	f = open(path, "r")
-	content = f.read()
+	for line in f.readlines():
+		line = line.strip()
+		if not (line.startswith('#') and not line.startswith('#(')):
+			content.append(line)
+		# else:
+		# 	print(line)
 	f.close()
+
+	content = '\n'.join(content)
 
 	for keyword in re.findall(r'#\((.+?)\)', content):
 		if not keyword in keywords \
@@ -41,7 +49,8 @@ else: authKey = os.environ['REVOKEAPPINSTANCEAUTHKEY']
 url = MOTHERSHIP + '/uploadTranslationKeywords'
 parameters = {"authorizationKey": authKey,
 			"keywords": ','.join(keywords),
+			"inline": 'true',
 			}
 data = urllib.parse.urlencode(parameters).encode('ascii')
 response = urllib.request.urlopen(url, data, context=sslcontext)
-print(response)
+print(response.read().decode())

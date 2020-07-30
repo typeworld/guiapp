7a2e49deeb38d2de5221c26b54e1f78010afaec0
Yanone <post@yanone.de>
2020-07-30 19:34:51 +0200

    av19

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-echo "Build Script"
-ls -la
 echo "App Build"
 python -m pip --version
 python wxPython/build/Mac/build-all.py
---
+++ install.sh
@@ -0,0 +1,4 @@
+python -m pip install -r requirements.txt
+curl https://github.com/sparkle-project/Sparkle/releases/download/1.23.0/Sparkle-1.23.0.tar.bz2 > sparkle.tar.bz
+tar -zxf sparkle.tar.bz
+ls -la

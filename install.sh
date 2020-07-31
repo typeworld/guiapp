2cbfe41bcb2a97451ac4055e3fed1c1ff48e8cdd
Yanone <post@yanone.de>
2020-07-31 13:51:56 +0200

    av26

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 python -m pip install -r requirements.txt
-curl -O -L https://github.com/sparkle-project/Sparkle/releases/download/1.23.0/Sparkle-1.23.0.tar.bz2
-mkdir sparkle
-tar -xf Sparkle-1.23.0.tar.bz2 --directory sparkle
-ls -la sparkle
+# curl -O -L https://github.com/sparkle-project/Sparkle/releases/download/1.23.0/Sparkle-1.23.0.tar.bz2
+# mkdir sparkle
+# tar -xf Sparkle-1.23.0.tar.bz2 --directory sparkle
 
 echo $GOOGLE_APPLICATION_CREDENTIALS_KEY
-echo ${GOOGLE_APPLICATION_CREDENTIALS_KEY/'\n'/'\\n'}" > "typeworld2-559c851e351b.json"
+echo ${GOOGLE_APPLICATION_CREDENTIALS_KEY/'\n'/'\\n'} > "typeworld2-559c851e351b.json"
 cat "typeworld2-559c851e351b.json"

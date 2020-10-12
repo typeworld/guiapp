5479aaa909605b4108a7f14ca7a3a974f9da6830
Yanone <post@yanone.de>
2020-10-12 12:07:08 +0200

    Formatting

--- wxPython/build/Mac/build-notarize.py
+++ wxPython/build/Mac/build-notarize.py
@@ -1,19 +1,20 @@
 import os, sys, time
-from subprocess import Popen,PIPE,STDOUT
+from subprocess import Popen, PIPE, STDOUT
 
 # List of commands as tuples of:
 # - Description
 # - Actual command
 # - True if this command is essential to the build process (must exit with 0), otherwise False
 
-def executeCommands(commands, printOutput = False, returnOutput = False):
+
+def executeCommands(commands, printOutput=False, returnOutput=False):
     for description, command, mustSucceed in commands:
 
         # Print which step we’re currently in
-        print(description, '...')
+        print(description, "...")
 
         # Execute the command, fetch both its output as well as its exit code
-        out = Popen(command, stderr=STDOUT,stdout=PIPE, shell=True)
+        out = Popen(command, stderr=STDOUT, stdout=PIPE, shell=True)
         output, exitcode = out.communicate()[0].decode(), out.returncode
 
         # If the exit code is not zero and this step is marked as necessary to succeed, print the output and quit the script.
@@ -23,7 +24,7 @@ def executeCommands(commands, printOutput = False, returnOutput = False):
             print(command)
             print()
             print('Step "%s" failed! See above.' % description)
-            print('Command used: %s' % command)
+            print("Command used: %s" % command)
             print()
             sys.exit(666)
         elif exitcode == 0 and returnOutput:
@@ -32,41 +33,56 @@ def executeCommands(commands, printOutput = False, returnOutput = False):
             print(output)
 
 
-notarization = executeCommands((
-   ('Upload for Notarization', f'xcrun altool --primary-bundle-id "Type.World" --notarize-app --username "post@yanone.de" --password "{os.environ["NOTARIZATION_PASSWORD"]}" --file dist/TypeWorldApp.dmg', True),
-), returnOutput=True)
+notarization = executeCommands(
+    (
+        (
+            "Upload for Notarization",
+            f'xcrun altool --primary-bundle-id "Type.World" --notarize-app --username "post@yanone.de" --password "{os.environ["NOTARIZATION_PASSWORD"]}" --file dist/TypeWorldApp.dmg',
+            True,
+        ),
+    ),
+    returnOutput=True,
+)
+
 
 RequestUUID = None
-for line in notarization.split('\n'):
-    if 'RequestUUID' in line:
-        RequestUUID = line.split('=')[1].strip()
-        print(f'Notarization UUID: {RequestUUID}')
+for line in notarization.split("\n"):
+    if "RequestUUID" in line:
+        RequestUUID = line.split("=")[1].strip()
+        print(f"Notarization UUID: {RequestUUID}")
 
 if not RequestUUID:
-    print('No RequestUUID returned')
+    print("No RequestUUID returned")
     sys.exit(1)
 
 while True:
 
     time.sleep(30)
 
-    check = executeCommands((
-        ('Check for notarization results', f'xcrun altool --notarization-info {RequestUUID} --username "post@yanone.de" --password "{os.environ["NOTARIZATION_PASSWORD"]}"', True),
-    ), returnOutput = True)
+    check = executeCommands(
+        (
+            (
+                "Check for notarization results",
+                f'xcrun altool --notarization-info {RequestUUID} --username "post@yanone.de" --password "{os.environ["NOTARIZATION_PASSWORD"]}"',
+                True,
+            ),
+        ),
+        returnOutput=True,
+    )
 
     if not RequestUUID in check:
-        print(f'No {RequestUUID} in xcrun altool --notarization-history')
+        print(f"No {RequestUUID} in xcrun altool --notarization-history")
         sys.exit(1)
 
-    if RequestUUID in check and 'Status: success' in check:
+    if RequestUUID in check and "Status: success" in check:
         sys.exit(0)
 
-    if RequestUUID in check and 'Status: invalid' in check:
+    if RequestUUID in check and "Status: invalid" in check:
         print(check)
         sys.exit(1)
 
-    if RequestUUID in check and 'Status: in progress' in check:
+    if RequestUUID in check and "Status: in progress" in check:
         pass
 
-print('Finished successfully.')
+print("Finished successfully.")
 print()

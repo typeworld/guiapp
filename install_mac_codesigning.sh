7c7a2b4b0895ee229dcb91242f514cef0b95187c
Yanone <post@yanone.de>
2020-10-13 10:39:00 +0200

    Additional commands

--- install_mac_codesigning.sh
+++ install_mac_codesigning.sh
@@ -17,6 +17,9 @@ security create-keychain -p travis $KEYCHAIN;
 # Make the custom keychain default, so xcodebuild will use it for signing
 security default-keychain -s $KEYCHAIN;
 
+# On top of that the keychain must be added to the search list:
+security list-keychains -s $KEYCHAIN
+
 # Unlock the keychain
 security unlock-keychain -p travis $KEYCHAIN;
 
@@ -28,6 +31,12 @@ security import wxPython/build/Mac/apple.cer -k ~/Library/Keychains/$KEYCHAIN -P
 security import wxPython/build/Mac/dac.cer -k ~/Library/Keychains/$KEYCHAIN -P travis -T /usr/bin/codesign
 # 3) Developer ID (That's you!)
 security import $CERTIFICATE_P12 -k $KEYCHAIN -P $MACOS_CERT_PASSWORD -P travis -T /usr/bin/codesign 2>&1 >/dev/null;
+
+# Then the keychain should be unlocked. Otherwise a prompt asking for the keychain password will be displayed:
+security unlock-keychain -p travis $KEYCHAIN
+
+# Eventually the auto-lock timeout should be disabled. This is in case the build is quite long and the keychain re-locks itself:
+security set-keychain-settings $KEYCHAIN
 
 # Let's delete the file, we no longer need it
 rm $CERTIFICATE_P12;

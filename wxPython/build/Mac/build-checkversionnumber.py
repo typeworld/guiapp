d8332432146b5da42205a468d8dba25ee41eff3a
Yanone <post@yanone.de>
2020-10-13 16:19:03 +0200

    Syntax error, formatting

--- wxPython/build/Mac/build-checkversionnumber.py
+++ wxPython/build/Mac/build-checkversionnumber.py
@@ -1,13 +1,10 @@
-import os
 import sys
+import typeworld.api
 
 version = sys.argv[-1]
 
-import typeworld.api
-
-desiredversion = '0.2.3-beta'
+desiredversion = "0.2.3-beta"
 
 if typeworld.api.VERSION != desiredversion:
-    print(
-        f"typeworld.api doesn't hold the up-to-date version number {desiredversion}")
+    print(f"typeworld.api doesn't hold the up-to-date version number {desiredversion}")
     sys.exit(1)

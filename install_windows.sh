f1f12a31153d24da2720330b4cc5d6667081b379
Yanone <post@yanone.de>
2020-10-13 17:50:16 +0200

    Update install_windows.sh

--- install_windows.sh
+++ install_windows.sh
@@ -1,2 +1,9 @@
+# Append PATH
+$env:PATH += ";$PYTHON"
+
+# Update pip
 $PYTHON\\python.exe -m pip install --upgrade pip
+
+# Install requirements
 $PYTHON\\python.exe -m pip install -r requirements_windows.txt
+

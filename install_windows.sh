286c55f22c3b7da11b90ad04ae18e20160503f99
Yanone <post@yanone.de>
2020-10-13 20:37:11 +0200

    Simplified

--- install_windows.sh
+++ install_windows.sh
@@ -1,6 +1,6 @@
 # Update pip
-$PYTHON\\python.exe -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 # Install requirements
-$PYTHON\\python.exe -m pip install -r requirements_windows.txt
+python -m pip install -r requirements_windows.txt
 

4d8f6e7a3df7ba779b2e4bfff310fbf4933a94c1
Yanone <post@yanone.de>
2020-10-13 18:02:09 +0200

    Windows

--- install_windows.sh
+++ install_windows.sh
@@ -1,8 +1,9 @@
 # Append PATH
 #$env:Path += ";$PYTHON;$PYTHON\\Scripts"
 echo $PATH
-$ENV:PATH="$ENV:PATH;$PYTHON"
-$ENV:PATH="$ENV:PATH;$PYTHON\\Scripts"
+$ENV:PATH="$ENV:PATH;$PYTHONPS"
+$ENV:PATH="$ENV:PATH;$PYTHONPS/Scripts"
+echo $PATH
 
 # Update pip
 $PYTHON\\python.exe -m pip install --upgrade pip

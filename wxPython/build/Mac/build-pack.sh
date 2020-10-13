07e2720a6657b7539d0f4920bc3e6b9e37868e7c
Yanone <post@yanone.de>
2020-10-13 11:12:09 +0200

    Removed unnecessary code

--- wxPython/build/Mac/build-pack.sh
+++ wxPython/build/Mac/build-pack.sh
@@ -9,16 +9,7 @@ xcrun --verbose stapler staple dist/Type.World.app
 echo "Create .dmg"
 dmgbuild -s wxPython/build/Mac/dmgbuild.py "Type.World App" dmg/TypeWorldApp.$APP_BUILD_VERSION.dmg
 
-echo "Sleep 5m"
-sleep 5m
-
-# echo "Upload preliminary dmg"
-# python wxPython/build/Mac/build-upload.py $APP_BUILD_VERSION
-
 echo "Sign .dmg"
-# xattr -cr dmg/TypeWorldApp.$APP_BUILD_VERSION.dmg
-# security unlock-keychain -p travis $KEYCHAIN;
-#codesign -vvvv -s "Developer ID Application: Jan Gerner" dmg/TypeWorldApp.$APP_BUILD_VERSION.dmg
 codesign -vvvv -s "Jan Gerner" dmg/TypeWorldApp.$APP_BUILD_VERSION.dmg
 
 echo "Verify .dmg"

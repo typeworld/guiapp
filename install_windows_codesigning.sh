3ee4c41321f0deea408ba0857d72e1095d659bf0
Yanone <post@yanone.de>
2020-10-14 16:24:38 +0200

    certs

--- install_windows_codesigning.sh
+++ install_windows_codesigning.sh
@@ -1,11 +1,17 @@
 # Decode the environment variable into our file
-echo $JANGERNER_CERT > jangerner.cer
-echo $CA_CERT > ca.cer
-python tweakcertificate.py jangerner.cer
-python tweakcertificate.py ca.cer
+echo $CERTUM_TRUSTED_NETWORK_CA > CERTUM_TRUSTED_NETWORK_CA.cer
+echo $SSL_COM_ROOT_CERTIFICATION_AUTHORITY_RSA > SSL_COM_ROOT_CERTIFICATION_AUTHORITY_RSA.cer
+echo $SSL_COM_CODE_SIGNING_INTERMEDIATE_CA_RSA_R1 > SSL_COM_CODE_SIGNING_INTERMEDIATE_CA_RSA_R1.cer
+echo $JANGERNER_CERT > JANGERNER_CERT.cer
+
+python tweakcertificate.py CERTUM_TRUSTED_NETWORK_CA.cer
+python tweakcertificate.py SSL_COM_ROOT_CERTIFICATION_AUTHORITY_RSA.cer
+python tweakcertificate.py SSL_COM_CODE_SIGNING_INTERMEDIATE_CA_RSA_R1.cer
+python tweakcertificate.py JANGERNER_CERT.cer
 
 # Add # https://stackoverflow.com/questions/84847/how-do-i-create-a-self-signed-certificate-for-code-signing-on-windows
-certutil -addstore "CA" ca.cer
-certutil -addstore -user -f "Jan Gerner" jangerner.cer
+certutil -addstore "CA" CERTUM_TRUSTED_NETWORK_CA.cer
+certutil -addstore "CA" SSL_COM_ROOT_CERTIFICATION_AUTHORITY_RSA.cer
+certutil -addstore "CA" SSL_COM_CODE_SIGNING_INTERMEDIATE_CA_RSA_R1.cer
+certutil -addstore -user -f "Jan Gerner" JANGERNER_CERT.cer
 
-exit 1

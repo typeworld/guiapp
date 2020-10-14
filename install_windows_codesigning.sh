1ced3abe9a135bb0e0790a266f4e42909e740cc3
Yanone <post@yanone.de>
2020-10-14 16:10:24 +0200

    Update install_windows_codesigning.sh

--- install_windows_codesigning.sh
+++ install_windows_codesigning.sh
@@ -6,7 +6,7 @@ echo $CERT_CER > $CERTIFICATE
 python tweakcertificate.py
 
 # Add # https://stackoverflow.com/questions/84847/how-do-i-create-a-self-signed-certificate-for-code-signing-on-windows
-#certutil -user -addstore Root $CERTIFICATE
-certutil -addstore -user -f "Jan Gerner" $CERTIFICATE
+certutil -addstore "Root" $CERTIFICATE
+#certutil -addstore -user -f "Jan Gerner" $CERTIFICATE
 
 exit 1

358d5c4db94b754a30cfe17a0f1dceb90cf30113
Yanone <post@yanone.de>
2020-10-14 13:41:50 +0200

    Update build-canupload.py

--- wxPython/build/build-canupload.py
+++ wxPython/build/build-canupload.py
@@ -1,28 +1,22 @@
 import os
 import sys
-import json
+import platform
 from google.cloud import storage
+
+WIN = platform.system() == "Windows"
 
 version = sys.argv[-2]
 platform = sys.argv[-1]
 ending = "dmg" if platform == "mac" else "exe"
 
-print("PWD:", os.environ["PWD"])
-
-# # Google Cloud Storage
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(
-#     os.environ["PWD"], "typeworld2-559c851e351b.json"
-# )
-
-keyfile = open(os.path.join(os.environ["PWD"], "typeworld2-559c851e351b.json"), "r")
-key = keyfile.read()
-keyfile.close()
-key = key.replace("\\\\n", "\\r\\n")
-keyfile = open(os.path.join(os.environ["PWD"], "typeworld2-559c851e351b.json"), "w")
-keyfile.write(key)
-keyfile.close()
-print(key)
-# print(json.loads(key))
+if WIN:
+    keyfile = open(os.path.join(os.environ["PWD"], "typeworld2-559c851e351b.json"), "r")
+    key = keyfile.read()
+    keyfile.close()
+    key = key.replace("\\\\n", "\\r\\n")
+    keyfile = open(os.path.join(os.environ["PWD"], "typeworld2-559c851e351b.json"), "w")
+    keyfile.write(key)
+    keyfile.close()
 
 storage_client = storage.Client.from_service_account_json(
     os.path.join(os.environ["PWD"], "typeworld2-559c851e351b.json")

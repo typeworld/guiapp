ca9c34e58940c08d879e13d8117ee076a24152d6
Yanone <post@yanone.de>
2020-10-14 10:24:04 +0200

    Update install_windows.sh

--- install_windows.sh
+++ install_windows.sh
@@ -4,3 +4,9 @@ python -m pip install --upgrade pip
 # Install requirements
 python -m pip install -r requirements_windows.txt
 
+# Google Cloud Storage Key
+echo $GOOGLE_APPLICATION_CREDENTIALS_KEY > "typeworld2-559c851e351b.json"
+cat "typeworld2-559c851e351b.json"
+
+# ynlib
+git clone https://github.com/yanone/ynlib.git

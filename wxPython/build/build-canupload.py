d74669f2cc70c19a0768379ab98c54476d2d9adf
Yanone <post@yanone.de>
2020-10-14 11:33:48 +0200

    read

--- wxPython/build/build-canupload.py
+++ wxPython/build/build-canupload.py
@@ -9,6 +9,8 @@ ending = "dmg" if platform == "mac" else "exe"
 # Google Cloud Storage
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "typeworld2-559c851e351b.json"
 
+print(open("typeworld2-559c851e351b.json", "r").read())
+
 storage_client = storage.Client()
 bucket = storage_client.bucket("typeworld2")
 blobPath = f"downloads/guiapp/TypeWorldApp.{version}.{ending}"

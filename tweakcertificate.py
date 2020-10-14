0bd5afa0c8d275418532669736a7a1dcb5633b42
Yanone <post@yanone.de>
2020-10-14 19:07:55 +0200

    old

--- tweakcertificate.py
+++ tweakcertificate.py
@@ -6,7 +6,7 @@ file = sys.argv[-1]
 keyfile = open(os.path.join(os.environ["PWD"], file), "r")
 key = keyfile.read()
 keyfile.close()
-key = key.replace("\\n", "\n")
+key = key.replace("\\n", "\r\n")
 keyfile = open(os.path.join(os.environ["PWD"], file), "w")
 keyfile.write(key)
 keyfile.close()

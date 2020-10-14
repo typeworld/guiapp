d1607a04748c8ffbd9d61d0119374e589f02aca7
Yanone <post@yanone.de>
2020-10-14 15:44:54 +0200

    Update tweakcertificate.py

--- tweakcertificate.py
+++ tweakcertificate.py
@@ -3,7 +3,7 @@ import os
 keyfile = open(os.path.join(os.environ["PWD"], os.environ["CERTIFICATE_CRT"]), "r")
 key = keyfile.read()
 keyfile.close()
-key = key.replace("\\\\n", "\\r\\n")
+key = key.replace("\\n", "\r\n")
 keyfile = open(os.path.join(os.environ["PWD"], os.environ["CERTIFICATE_CRT"]), "w")
 keyfile.write(key)
 keyfile.close()

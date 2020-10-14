677fe1f227aa6935398e270c21157bd6beae75b5
Yanone <post@yanone.de>
2020-10-14 11:31:25 +0200

    env

--- wxPython/build/build-upload.py
+++ wxPython/build/build-upload.py
@@ -1,9 +1,13 @@
+import os
 import sys
 from google.cloud import storage
 
 version = sys.argv[-1]
 platform = sys.argv[-1]
 ending = "dmg" if platform == "mac" else "exe"
+
+# Upload
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "typeworld2-559c851e351b.json"
 
 storage_client = storage.Client()
 bucket = storage_client.bucket("typeworld2")

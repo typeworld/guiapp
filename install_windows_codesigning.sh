5e43b809b4fbaf00716281545dfa5fb941dfce7f
Yanone <post@yanone.de>
2020-10-14 17:38:37 +0200

    Update install_windows_codesigning.sh

--- install_windows_codesigning.sh
+++ install_windows_codesigning.sh
@@ -14,9 +14,9 @@ python tweakcertificate.py JANGERNER_CERT.cer
 # https://stackoverflow.com/questions/28690986/code-signing-with-signtool-fails-due-to-private-key-filter
 
 certutil -addstore "Root" CERTUM_TRUSTED_NETWORK_CA.cer
-certutil -addstore -user -f "CA" SSL_COM_ROOT_CERTIFICATION_AUTHORITY_RSA.cer
-certutil -addstore -user -f "CA" SSL_COM_CODE_SIGNING_INTERMEDIATE_CA_RSA_R1.cer
-certutil -addstore -user -f "My" JANGERNER_CERT.cer
+certutil -addstore "CA" SSL_COM_ROOT_CERTIFICATION_AUTHORITY_RSA.cer
+certutil -addstore "CA" SSL_COM_CODE_SIGNING_INTERMEDIATE_CA_RSA_R1.cer
+certutil -addstore -user "My" JANGERNER_CERT.cer
 
 # Certmgr.exe /c /add CERTUM_TRUSTED_NETWORK_CA.cer /s root
 # Certmgr.exe /c /add SSL_COM_ROOT_CERTIFICATION_AUTHORITY_RSA.cer /s ca

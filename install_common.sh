89f809844cbf9a58ae47259923876827030188f7
Yanone <post@yanone.de>
2020-10-14 13:56:03 +0200

    No need to export this as the file will be called directly in the code

--- install_common.sh
+++ install_common.sh
@@ -1,10 +1,5 @@
 # Google Cloud Storage Key
 echo $GOOGLE_APPLICATION_CREDENTIALS_KEY > "typeworld2-559c851e351b.json"
-# Unix
-export GOOGLE_APPLICATION_CREDENTIALS=typeworld2-559c851e351b.json
-# Windows
-set GOOGLE_APPLICATION_CREDENTIALS=typeworld2-559c851e351b.json
-echo "Credentials file: $GOOGLE_APPLICATION_CREDENTIALS"
 
 # ynlib
 git clone https://github.com/yanone/ynlib.git

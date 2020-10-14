6b213bbcb56b3fe9d3bb25ba699a60f97b350fd9
Yanone <post@yanone.de>
2020-10-14 19:29:58 +0200

    base32

---
+++ base32decode.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import base64
+
+file = sys.argv[-1]
+
+keyfile = open(os.path.join(os.environ["PWD"], file), "rb")
+key = keyfile.read()
+keyfile.close()
+key = base64.b32decode(key)
+keyfile = open(os.path.join(os.environ["PWD"], file), "wb")
+keyfile.write(key)
+keyfile.close()
--- install_windows_codesigning.sh
+++ install_windows_codesigning.sh
@@ -6,8 +6,8 @@ echo $JANGERNER_CERT > JANGERNER_CERT.cer
 # echo $JANGERNER_P12 > JANGERNER.p12.base64encoded
 # python tweakcertificate.py JANGERNER.p12.base64encoded
 # python -m base64 -d JANGERNER.p12.base64encoded > JANGERNER.p12
-echo $JANGERNER_P12 | python -m base64 -d > JANGERNER.p12
-python -m base64 JANGERNER.p12
+echo $JANGERNER_P12 > JANGERNER.p12
+python base32decode.py JANGERNER.p12
 # certutil -decode JANGERNER.p12.base64encoded JANGERNER.p12
 # base64 -d JANGERNER.p12.base64encoded > JANGERNER.p12
 # echo $JANGERNER_P12 | base64 -d - > JANGERNER.p12

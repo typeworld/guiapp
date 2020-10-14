d8a9fb965b9195939bb8595b6046b4a9057e35ed
Yanone <post@yanone.de>
2020-10-14 16:17:17 +0200

    certs

--- install_windows_codesigning.sh
+++ install_windows_codesigning.sh
@@ -1,12 +1,11 @@
-# Set the filename
-export CERTIFICATE=cert.cer;
-
 # Decode the environment variable into our file
-echo $CERT_CER > $CERTIFICATE
-python tweakcertificate.py
+echo $JANGERNER_CERT > jangerner.cer
+echo $CA_CERT > ca.cer
+python tweakcertificate.py jangerner.cer
+python tweakcertificate.py ca.cer
 
 # Add # https://stackoverflow.com/questions/84847/how-do-i-create-a-self-signed-certificate-for-code-signing-on-windows
-certutil -addstore "Root" $CERTIFICATE
-#certutil -addstore -user -f "Jan Gerner" $CERTIFICATE
+certutil -addstore "CA" ca.cer
+certutil -addstore -user -f "Jan Gerner" jangerner.cer
 
 exit 1
--- tweakcertificate.py
+++ tweakcertificate.py
@@ -1,9 +1,12 @@
 import os
+import sys
 
-keyfile = open(os.path.join(os.environ["PWD"], os.environ["CERTIFICATE"]), "r")
+file = sys.argv[-1]
+
+keyfile = open(os.path.join(os.environ["PWD"], file), "r")
 key = keyfile.read()
 keyfile.close()
 key = key.replace("\\n", "\r\n")
-keyfile = open(os.path.join(os.environ["PWD"], os.environ["CERTIFICATE"]), "w")
+keyfile = open(os.path.join(os.environ["PWD"], file), "w")
 keyfile.write(key)
 keyfile.close()

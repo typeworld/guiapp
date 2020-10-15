f2edd0dc88d874deb27c6d30df5c257f82ab099d
Yanone <post@yanone.de>
2020-10-15 10:25:22 +0200

    Update build-main.sh

--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -12,11 +12,18 @@ echo "Add Windows App Manifest"
 echo "Copy Google Code"
 dir "$SITEPACKAGES"
 #xcopy "$SITEPACKAGES\\googleapis_common_protos-*.dist-info" "build\\lib\\" /s /e /h /i /y
-xcopy "$SITEPACKAGES\\google" "build\\lib\\google" /s /y
+echo 1
+pushd build\\lib\\
+xcopy "$SITEPACKAGES\\google" "google" /s /y
+echo 2
 xcopy "$SITEPACKAGES\\googleapis_common_protos-1.52.0.dist-info" "build\\lib\\googleapis_common_protos-1.52.0.dist-info" /s /y
+echo 3
 xcopy "$SITEPACKAGES\\google_api_core-1.22.4.dist-info" "build\\lib\\google_api_core-1.22.4.dist-info" /s /y
+echo 4
 xcopy "$SITEPACKAGES\\google_auth-1.22.1.dist-info" "build\\lib\\google_auth-1.22.1.dist-info" /s /y
+echo 5
 xcopy "$SITEPACKAGES\\google_cloud_pubsub-2.1.0.dist-info" "build\\lib\\google_cloud_pubsub-2.1.0.dist-info" /s /y
+popd
 
 echo "Copy ynlib"
 xcopy ynlib "build\\lib\\ynlib" /s /y

47f082953abac4080e85de944c88c3665fac2108
Yanone <post@yanone.de>
2020-10-15 10:06:57 +0200

    Update build-main.sh

--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -11,21 +11,19 @@ echo "Add Windows App Manifest"
 
 echo "Copy Google Code"
 dir "$SITEPACKAGES"
-echo "-$SITEPACKAGES-"
 #xcopy "$SITEPACKAGES\\googleapis_common_protos-*.dist-info" "build\\lib\\" /s /e /h /i /y
-echo "$SITEPACKAGES\\googleapis_common_protos-1.52.0.dist-info" "build\\lib\\googleapis_common_protos-1.52.0.dist-info" /s /e /h 
-xcopy "$SITEPACKAGES\\googleapis_common_protos-1.52.0.dist-info" "build\\lib\\googleapis_common_protos-1.52.0.dist-info" /s /e /h 
 xcopy "$SITEPACKAGES\\google" "build\\lib\\google" /s /e /h
-xcopy "$SITEPACKAGES\\google_api_core-1.22.4.dist-info" "build\\lib\\google_api_core-1.22.4.dist-info" /s /e /h
-xcopy "$SITEPACKAGES\\google_auth-1.22.1.dist-info" "build\\lib\\google_auth-1.22.1.dist-info" /s /e /h
-xcopy "$SITEPACKAGES\\google_cloud_pubsub-2.1.0.dist-info" "build\\lib\\google_cloud_pubsub-2.1.0.dist-info" /s /e /h
+xcopy "$SITEPACKAGES\\googleapis_common_protos-1.52.0.dist-info" "build\\lib\\googleapis_common_protos-1.52.0.dist-info"
+xcopy "$SITEPACKAGES\\google_api_core-1.22.4.dist-info" "build\\lib\\google_api_core-1.22.4.dist-info"
+xcopy "$SITEPACKAGES\\google_auth-1.22.1.dist-info" "build\\lib\\google_auth-1.22.1.dist-info"
+xcopy "$SITEPACKAGES\\google_cloud_pubsub-2.1.0.dist-info" "build\\lib\\google_cloud_pubsub-2.1.0.dist-info"
 
 echo "Copy ynlib"
-xcopy ynlib "build\\lib\\ynlib" /s /e /h
+xcopy ynlib "build\\lib\\ynlib"
 
 echo "Copy importlib_metadata"
 xcopy "$SITEPACKAGES\\importlib_metadata" "build\\lib\\importlib_metadata"  /s /e /h
-xcopy "$SITEPACKAGES\\importlib_metadata-1.7.0.dist-info" "build\\lib\\importlib_metadata-1.7.0.dist-info"  /s /e /h
+xcopy "$SITEPACKAGES\\importlib_metadata-1.7.0.dist-info" "build\\lib\\importlib_metadata-1.7.0.dist-info"
 
 echo "Signing TypeWorld.exe"
 "$WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /debug /td sha256 /fd SHA256 /f jan_gerner.p12 /p $JANGERNER_P12_PASSWORD "build\\TypeWorld.exe"

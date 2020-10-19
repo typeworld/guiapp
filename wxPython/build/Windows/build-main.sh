54b70a2ecc1f299486244884adf1905c2d57667c
Yanone <post@yanone.de>
2020-10-19 10:37:59 +0200

    xcopy

--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -11,26 +11,23 @@ echo "Add Windows App Manifest"
 
 echo "Copy Google Code"
 dir "$SITEPACKAGES"
-#xcopy "$SITEPACKAGES\\googleapis_common_protos-*.dist-info" "build\\lib\\" /s /e /h /i /y
 echo 1
-#pushd build\\lib\\
-xcopy %SITEPACKAGES%\\google build\\lib\\ /s /y
+xcopy "$SITEPACKAGES\\google" "build\\lib\\google" /i /e /h
 echo 2
-xcopy "$SITEPACKAGES\\googleapis_common_protos-1.52.0.dist-info" "build\\lib\\googleapis_common_protos-1.52.0.dist-info" /s /y
+xcopy "$SITEPACKAGES\\googleapis_common_protos-1.52.0.dist-info" "build\\lib\\googleapis_common_protos-1.52.0.dist-info" /i /e /h
 echo 3
-xcopy "$SITEPACKAGES\\google_api_core-1.22.4.dist-info" "build\\lib\\google_api_core-1.22.4.dist-info" /s /y
+xcopy "$SITEPACKAGES\\google_api_core-1.22.4.dist-info" "build\\lib\\google_api_core-1.22.4.dist-info" /i /e /h
 echo 4
-xcopy "$SITEPACKAGES\\google_auth-1.22.1.dist-info" "build\\lib\\google_auth-1.22.1.dist-info" /s /y
+xcopy "$SITEPACKAGES\\google_auth-1.22.1.dist-info" "build\\lib\\google_auth-1.22.1.dist-info" /i /e /h
 echo 5
-xcopy "$SITEPACKAGES\\google_cloud_pubsub-2.1.0.dist-info" "build\\lib\\google_cloud_pubsub-2.1.0.dist-info" /s /y
-#popd
+xcopy "$SITEPACKAGES\\google_cloud_pubsub-2.1.0.dist-info" "build\\lib\\google_cloud_pubsub-2.1.0.dist-info" /i /e /h
 
 echo "Copy ynlib"
-xcopy ynlib "build\\lib\\ynlib" /s /y
+xcopy ynlib "build\\lib\\ynlib" /i /e /h
 
 echo "Copy importlib_metadata"
-xcopy "$SITEPACKAGES\\importlib_metadata" "build\\lib\\importlib_metadata"  /s /e /h
-xcopy "$SITEPACKAGES\\importlib_metadata-1.7.0.dist-info" "build\\lib\\importlib_metadata-1.7.0.dist-info"
+xcopy "$SITEPACKAGES\\importlib_metadata" "build\\lib\\importlib_metadata"  /i /e /h
+xcopy "$SITEPACKAGES\\importlib_metadata-1.7.0.dist-info" "build\\lib\\importlib_metadata-1.7.0.dist-info" /i /e /h
 
 echo "Signing TypeWorld.exe"
 "$WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /debug /td sha256 /fd SHA256 /f jan_gerner.p12 /p $JANGERNER_P12_PASSWORD "build\\TypeWorld.exe"

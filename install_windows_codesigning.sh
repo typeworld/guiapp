b22834f09173380bc87206ef96ff0e01c5024151
Yanone <post@yanone.de>
2020-10-19 14:25:14 +0200

    Update install_windows_codesigning.sh

--- install_windows_codesigning.sh
+++ install_windows_codesigning.sh
@@ -38,7 +38,7 @@ certutil -addstore -user "My" JANGERNER_CERT.cer
 
 # https://stackoverflow.com/questions/5171117/import-pfx-file-into-particular-certificate-store-from-command-line
 #certutil -f -p $JANGERNER_P12_PASSWORD -importpfx My "jangerner.pfx"
-certutil -user -p $JANGERNER_P12_PASSWORD -importPFX jangerner.pfx
+certutil -user -p $JANGERNER_P12_PASSWORD -importPFX jangerner.pfx -enterprise
 
 # Certmgr.exe /c /add CERTUM_TRUSTED_NETWORK_CA.cer /s root
 # Certmgr.exe /c /add SSL_COM_ROOT_CERTIFICATION_AUTHORITY_RSA.cer /s ca

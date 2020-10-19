95f8285134515a35bf7427ecc83728b53b86df9d
Yanone <post@yanone.de>
2020-10-19 13:09:42 +0200

    try this

--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -39,7 +39,8 @@ echo "Copy importlib_metadata"
 python -c "import shutil; shutil.copytree('$SITEPACKAGES\\importlib_metadata-1.7.0.dist-info', 'build\\lib\\importlib_metadata-1.7.0.dist-info')"
 
 echo "Signing TypeWorld.exe"
-dir . 
+dir .
+# Export key from Windows: https://www.ca.kit.edu/129.php 
 "$WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /debug /td sha256 /fd SHA256 /f jangerner.pfx /p $JANGERNER_P12_PASSWORD "build\\TypeWorld.exe"
 # $WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /debug /td sha256 /fd SHA256 /n "Jan Gerner" "build\\TypeWorld.exe"',
 

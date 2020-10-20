4134cd06b2fca02857297f135d1e699fc1796e4d
Yanone <post@yanone.de>
2020-10-20 15:16:17 +0200

    Update build-main.sh

--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -40,14 +40,16 @@ python -c "import shutil; shutil.copytree('$SITEPACKAGES\\importlib_metadata-1.7
 
 echo "Signing TypeWorld.exe"
 # Export key from Windows: https://www.ca.kit.edu/129.php 
-echo "$WINDOWSKITBIN"\\signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld.exe"
-"$WINDOWSKITBIN"\\signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld.exe"
+python -c "import os; os.system('\"$WINDOWSKITBIN\\signtool.exe\" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n \"Jan Gerner\" /v /debug \"build\\TypeWorld.exe\"')"
+#echo "$WINDOWSKITBIN"\\signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld.exe"
+#"$WINDOWSKITBIN"\\signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld.exe"
 #"$WINDOWSKITBIN\\signtool.exe" sign /v /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /f jangerner.pfx /p $JANGERNER_P12_PASSWORD "build\\TypeWorld.exe"
 
 certutil -user -p $JANGERNER_P12_PASSWORD -importPFX jangerner.pfx NoRoot
 
 echo "Signing TypeWorld Subscription Opener.exe"
-"$WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld Subscription Opener.exe"
+python -c "import os; os.system('\"$WINDOWSKITBIN\\signtool.exe\" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n \"Jan Gerner\" /v /debug \"build\\TypeWorld Subscription Opener.exe\"')"
+#"$WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld Subscription Opener.exe"
 #"$WINDOWSKITBIN\\signtool.exe" sign /v /tr http://timestamp.digicert.com /debug /td sha256 /fd SHA256 /f jangerner.pfx /p $JANGERNER_P12_PASSWORD "build\\TypeWorld Subscription Opener.exe"
 
 echo "Verify signature"

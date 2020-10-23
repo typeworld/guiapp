b3981ae9d2d78d0c568d07f3dbeb4ae517d7585e
Yanone <post@yanone.de>
2020-10-23 10:31:48 +0200

    Update build-all.sh

--- wxPython/build/Windows/build-all.sh
+++ wxPython/build/Windows/build-all.sh
@@ -1,6 +1,9 @@
 set -e
 
-export TYPEWORLD_APIKEY=$(python -c "import os; print(os.getenv('TYPEWORLD_APIKEY'))")
+# # Copy env from python to bash
+# export TYPEWORLD_APIKEY=$(python -c "import os; print(os.getenv('TYPEWORLD_APIKEY'))")
+# export JANGERNER_P12_PASSWORD=$(python -c "import os; print(os.getenv('JANGERNER_P12_PASSWORD'))")
+
 export APP_BUILD_VERSION=$(curl "https://api.type.world/latestUnpublishedVersion/world.type.guiapp/windows/?TYPEWORLD_APIKEY=$TYPEWORLD_APIKEY")
 export SITEPACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`\\Lib\\site-packages
 export WINDOWSKITBIN="C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86"

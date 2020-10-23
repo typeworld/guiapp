4e2b17f3738ef34003284e864b895dbeb9b6d4af
Yanone <post@yanone.de>
2020-10-23 19:13:29 +0200

    s

--- wxPython/build/Windows/build-sign.bat
+++ wxPython/build/Windows/build-sign.bat
@@ -1,7 +1,5 @@
 echo "Signing TypeWorld.exe"
-"C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86\\signtool.exe"
-echo "Signing TypeWorld.exe"
-"C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v "build\\TypeWorld.exe"
+"%WINDOWSKITBIN%\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v "build\\TypeWorld.exe"
 
 echo "Signing TypeWorld Subscription Opener.exe"
 "%WINDOWSKITBIN%\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld Subscription Opener.exe"

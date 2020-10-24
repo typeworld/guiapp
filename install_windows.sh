e7f406efbfcc2a2c9791213fdbb993d06ae1d23c
Yanone <post@yanone.de>
2020-10-24 11:20:07 +0200

    Downloads Sparkle

--- install_windows.sh
+++ install_windows.sh
@@ -9,3 +9,8 @@ python -m pip install --upgrade pip
 
 # Install requirements
 python -m pip install -r requirements_windows.txt
+
+# Download Sparkle
+curl -O -L https://github.com/vslavik/winsparkle/releases/download/v0.7.0/WinSparkle-0.7.0.zip
+mkdir sparkle
+unzip WinSparkle-0.7.0.zip -d sparkle

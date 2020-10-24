10a6efeadf3488209440df7c4e3185ec9e576f36
Yanone <post@yanone.de>
2020-10-24 09:56:14 +0200

    Update build-pack.sh

--- wxPython/build/Windows/build-pack.sh
+++ wxPython/build/Windows/build-pack.sh
@@ -1,3 +1,7 @@
+export APP_BUILD_VERSION=$(curl "https://api.type.world/latestUnpublishedVersion/world.type.guiapp/windows/?TYPEWORLD_APIKEY=$TYPEWORLD_APIKEY")
+export SITEPACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`\\Lib\\site-packages
+export WINDOWSKITBIN="C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86"
+
 echo "Create InnoSetup .iss file"
 python "wxPython/build/Windows/createissfile.py"
 

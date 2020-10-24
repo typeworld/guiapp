4155ad5e8ef7525dd8c1a136724f7be1dbf90b98
Yanone <post@yanone.de>
2020-10-24 08:08:00 +0200

    cleanup

--- wxPython/build/Windows/build-all.sh
+++ wxPython/build/Windows/build-all.sh
@@ -1,16 +1,8 @@
 set -e
 
-# # Copy env from python to bash
-# export TYPEWORLD_APIKEY=$(python -c "import os; print(os.getenv('TYPEWORLD_APIKEY'))")
-# export JANGERNER_P12_PASSWORD=$(python -c "import os; print(os.getenv('JANGERNER_P12_PASSWORD'))")
-
 export APP_BUILD_VERSION=$(curl "https://api.type.world/latestUnpublishedVersion/world.type.guiapp/windows/?TYPEWORLD_APIKEY=$TYPEWORLD_APIKEY")
 export SITEPACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`\\Lib\\site-packages
 export WINDOWSKITBIN="C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86"
-#10.0.19041.0
-#10.0.18362.0
-echo $SITEPACKAGES
-#dir $SITEPACKAGES
 
 echo "Check if typeworld.api holds correct version number"
 python wxPython/build/build-checkversionnumber.py
@@ -22,7 +14,6 @@ python wxPython/build/build-canupload.py windows
 # console instead of popup message windows, to read output of self test in the build
 echo "Main Build, Console base"
 export BUILDBASE="Console"
-# cmd.exe /C .\\wxPython\\build\\Windows\\build-main.bat
 $SHELL wxPython/build/Windows/build-main.sh
 
 # Clear build folder
--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -1,7 +1,5 @@
 set -e
 
-dir $SITEPACKAGES
-
 echo "Python build"
 python wxPython/build/Windows/setup.py build > nul # muting too much output
 
@@ -12,76 +10,17 @@ echo "Add Windows App Manifest"
 "$WINDOWSKITBIN\\mt.exe" -manifest "wxPython/build/Windows/windowsAppManifest.xml" -outputresource:build\\TypeWorld.exe;#1
 
 echo "Copy Google Code"
-# Using Python for copying instead of xcopy as I couldn't get it to copy on AppVeyor.
-# https://stackoverflow.com/questions/64424550/xcopy-fails-in-appveyor-works-at-home-invalid-number-of-parameters
-echo 1
-#xcopy "$SITEPACKAGES\\google\\" "build\\lib\\google\\" /i /e /h
-# python -c "import shutil; shutil.copytree('$SITEPACKAGES\\google', 'build\\lib\\google')"
 cp -r $SITEPACKAGES/google build/lib/
-echo 2
-#xcopy "$SITEPACKAGES\\googleapis_common_protos-1.52.0.dist-info\\" "build\\lib\\googleapis_common_protos-1.52.0.dist-info\\" /i /e /h
-# python -c "import shutil; shutil.copytree('$SITEPACKAGES\\googleapis_common_protos-1.52.0.dist-info', 'build\\lib\\googleapis_common_protos-1.52.0.dist-info')"
 cp -r $SITEPACKAGES/googleapis_common_protos-*.dist-info build/lib/
-echo 3
-#xcopy "$SITEPACKAGES\\google_api_core-1.22.4.dist-info" "build\\lib\\google_api_core-1.22.4.dist-info" /i /e /h
-# python -c "import shutil; shutil.copytree('$SITEPACKAGES\\google_api_core-1.23.0.dist-info', 'build\\lib\\google_api_core-1.23.0.dist-info')"
 cp -r $SITEPACKAGES/google_api_core-*.dist-info build/lib/
-echo 4
-#xcopy "$SITEPACKAGES\\google_auth-1.22.1.dist-info" "build\\lib\\google_auth-1.22.1.dist-info" /i /e /h
-# python -c "import shutil; shutil.copytree('$SITEPACKAGES\\google_auth-1.22.1.dist-info', 'build\\lib\\google_auth-1.22.1.dist-info')"
 cp -r $SITEPACKAGES/google_auth-*.dist-info build/lib/
-echo 5
-#xcopy "$SITEPACKAGES\\google_cloud_pubsub-2.1.0.dist-info" "build\\lib\\google_cloud_pubsub-2.1.0.dist-info" /i /e /h
-# python -c "import shutil; shutil.copytree('$SITEPACKAGES\\google_cloud_pubsub-2.1.0.dist-info', 'build\\lib\\google_cloud_pubsub-2.1.0.dist-info')"
 cp -r $SITEPACKAGES/google_cloud_pubsub-*.dist-info build/lib/
 
 echo "Copy ynlib"
-#xcopy ynlib "build\\lib\\ynlib" /i /e /h
-# python -c "import shutil; shutil.copytree('ynlib\\Lib\\ynlib', 'build\\lib\\ynlib')"
 cp -r ynlib/Lib/ynlib build/lib/
 
 echo "Copy importlib_metadata"
-#xcopy "$SITEPACKAGES\\importlib_metadata" "build\\lib\\importlib_metadata"  /i /e /h
-# python -c "import shutil; shutil.copytree('$SITEPACKAGES\\importlib_metadata', 'build\\lib\\importlib_metadata')"
-#xcopy "$SITEPACKAGES\\importlib_metadata-1.7.0.dist-info" "build\\lib\\importlib_metadata-1.7.0.dist-info" /i /e /h
-# python -c "import shutil; shutil.copytree('$SITEPACKAGES\\importlib_metadata-1.7.0.dist-info', 'build\\lib\\importlib_metadata-1.7.0.dist-info')"
 cp -r $SITEPACKAGES/importlib_metadata-*.dist-info build/lib/
 
-dir build\\lib
-
-# echo "Signing TypeWorld.exe"
-# # Export key from Windows: https://www.ca.kit.edu/129.php 
-# # python -c "import os; command = '\"$WINDOWSKITBIN\\signtool.exe\" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n \"Jan Gerner\" /v /debug \"build\\TypeWorld.exe\"'; print(command); os.system(command)"
-# # echo "$WINDOWSKITBIN"\\signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld.exe"
-# #"$WINDOWSKITBIN\\signtool.exe" sign /v /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /f jangerner.pfx /p $JANGERNER_P12_PASSWORD "build\\TypeWorld.exe"
-# cmd.exe /C ""$WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld.exe""
-
-# #certutil -user -p $JANGERNER_P12_PASSWORD -importPFX jangerner.pfx NoRoot
-
-# echo "Signing TypeWorld Subscription Opener.exe"
-# #python -c "import os; os.system('\"$WINDOWSKITBIN\\signtool.exe\" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n \"Jan Gerner\" /v /debug \"build\\TypeWorld Subscription Opener.exe\"')"
-# #"$WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld Subscription Opener.exe"
-# "$WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v /debug "build\\TypeWorld Subscription Opener.exe"
-
-# echo "Verify signature"
-# "$WINDOWSKITBIN\\signtool.exe" verify /pa /v "build\\TypeWorld.exe"
-# "$WINDOWSKITBIN\\signtool.exe" verify /pa /v "build\\TypeWorld Subscription Opener.exe"
-
-
-#     # if "agent" in profile:
-#     #     executeCommands(
-#     #         [
-#     #             [
-#     #                 "Signing TypeWorld Taskbar Agent.exe",
-#     #                 $WINDOWSKITBIN\\signtool.exe" sign /tr http://timestamp.digicert.com /debug /td sha256 /fd SHA256 /a /n "Jan Gerner" "build\\TypeWorld Taskbar Agent.exe"',
-#     #                 True,
-#     #             ],
-#     #             [
-#     #                 "Verify signature",
-#     #                 $WINDOWSKITBIN\\signtool.exe" verify /pa /v "build\\TypeWorld Taskbar Agent.exe"',
-#     #                 True,
-#     #             ],
-#     #         ]        )
-
-# echo "App Self Test"
-# "build\\TypeWorld.exe" selftest
+echo "App Self Test"
+"build/TypeWorld.exe" selftest

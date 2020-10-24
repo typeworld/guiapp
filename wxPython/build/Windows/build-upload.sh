9d94155ec5ed8035dc25308f8435057a3d95ff85
Yanone <post@yanone.de>
2020-10-24 10:36:15 +0200

    upload

---
+++ wxPython/build/Windows/build-upload.sh
@@ -0,0 +1,6 @@
+export APP_BUILD_VERSION=$(curl "https://api.type.world/latestUnpublishedVersion/world.type.guiapp/windows/?TYPEWORLD_APIKEY=$TYPEWORLD_APIKEY")
+export SITEPACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`\\Lib\\site-packages
+export WINDOWSKITBIN="C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86"
+
+python wxPython/build/build-upload.py windows
+python wxPython/build/Windows/build-uploadsignature.py

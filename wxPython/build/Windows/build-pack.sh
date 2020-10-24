29c84ddc4fd81b47fbfc01546992d5c4335a6c30
Yanone <post@yanone.de>
2020-10-24 08:58:07 +0200

    Update build-pack.sh

--- wxPython/build/Windows/build-pack.sh
+++ wxPython/build/Windows/build-pack.sh
@@ -2,4 +2,4 @@ echo "Create InnoSetup .iss file"
 python "wxPython/build/Windows/createissfile.py"
 
 echo "Create InnoSetup Installer"
-"C:/Program Files (x86)/Inno Setup 5/iscc.exe" "wxPython/build/Windows/TypeWorld.iss"
+"/C/Program Files (x86)/Inno Setup 5/iscc.exe" "wxPython/build/Windows/TypeWorld.iss"

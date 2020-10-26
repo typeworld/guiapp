9afe1ec36cac3aaeefe7ae36b70d1afc4458795e
Yanone <post@yanone.de>
2020-10-26 12:27:27 +0100

    syntax

--- wxPython/app.py
+++ wxPython/app.py
@@ -86,24 +86,34 @@ if WIN:
         DEBUG = True
 
     # Redirect stderr & stdout
-    try: 
-        sys.stdout.write("\n") 
-        sys.stdout.flush() 
-    except IOError: 
-        class dummyStream: 
-        ''' dummyStream behaves like a stream but does nothing. ''' 
-            def __init__(self): pass 
-            def write(self,data): pass 
-            def read(self,data): pass 
-            def flush(self): pass 
-            def close(self): pass 
-        # and now redirect all default streams to this dummyStream: 
-        sys.stdout = dummyStream() 
-        sys.stderr = dummyStream() 
-        sys.stdin = dummyStream() 
-        sys.__stdout__ = dummyStream() 
-        sys.__stderr__ = dummyStream() 
-        sys.__stdin__ = dummyStream() 
+    try:
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+    except IOError:
+
+        class dummyStream:
+            def __init__(self):
+                pass
+
+            def write(self, data):
+                pass
+
+            def read(self, data):
+                pass
+
+            def flush(self):
+                pass
+
+            def close(self):
+                pass
+
+        # and now redirect all default streams to this dummyStream:
+        sys.stdout = dummyStream()
+        sys.stderr = dummyStream()
+        sys.stdin = dummyStream()
+        sys.__stdout__ = dummyStream()
+        sys.__stderr__ = dummyStream()
+        sys.__stdin__ = dummyStream()
 
 global app
 app = None

5ad14185d984187969c54ee61bf35eab20ee6c10
Yanone <post@yanone.de>
2020-10-26 09:31:12 +0100

    Update build-signinstaller.bat

--- wxPython/build/Windows/build-signinstaller.bat
+++ wxPython/build/Windows/build-signinstaller.bat
@@ -1,7 +1,7 @@
 dir dmg
 
 echo "Signing Installer Package"
-"C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v "dmg\\TypeWorldApp.%APP_BUILD_VERSION%.exe"
+"C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86\\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd SHA256 /n "Jan Gerner" /v "dmg\\TypeWorldApp.exe"
 
 echo "Verify signature"
-"C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86\\signtool.exe" verify /pa /v "dmg\\TypeWorldApp.%APP_BUILD_VERSION%.exe"
+"C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x86\\signtool.exe" verify /pa /v "dmg\\TypeWorldApp.exe"

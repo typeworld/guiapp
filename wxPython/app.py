586835cd3e67aeebd8cb730723c2b0b05eacfb9c
Yanone <post@yanone.de>
2020-10-26 12:22:05 +0100

    syntax error

--- wxPython/app.py
+++ wxPython/app.py
@@ -92,11 +92,11 @@ if WIN:
     except IOError: 
         class dummyStream: 
         ''' dummyStream behaves like a stream but does nothing. ''' 
-        def __init__(self): pass 
-        def write(self,data): pass 
-        def read(self,data): pass 
-        def flush(self): pass 
-        def close(self): pass 
+            def __init__(self): pass 
+            def write(self,data): pass 
+            def read(self,data): pass 
+            def flush(self): pass 
+            def close(self): pass 
         # and now redirect all default streams to this dummyStream: 
         sys.stdout = dummyStream() 
         sys.stderr = dummyStream() 

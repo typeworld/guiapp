823ef0b3c52de1be3ca741da4d1091322ac2f20d
Yanone <post@yanone.de>
2020-10-27 08:41:50 +0100

    print to stderr

--- wxPython/build/build-canbuild.sh
+++ wxPython/build/build-canbuild.sh
@@ -3,7 +3,7 @@ echo "Checking build version number availability for $1 platform"
 APP_BUILD_VERSION=$(curl "https://api.type.world/latestUnpublishedVersion/world.type.guiapp/$1/?TYPEWORLD_APIKEY=$TYPEWORLD_APIKEY")
 
 if [[ "$APP_BUILD_VERSION" == "n/a" ]]; then
-    echo "No build version number available for $1 platform: n/a"
+    echo "No build version number available for $1 platform: n/a" 1>&2
     exit 1
 else
     echo "Building version $APP_BUILD_VERSION for $1 platform"

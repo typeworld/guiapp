5e09d9d280228cf57bcc3038bcaabb21fe055f54
Yanone <post@yanone.de>
2020-10-27 08:30:20 +0100

    Try with shell script

---
+++ wxPython/build/build-canbuild.sh
@@ -0,0 +1,8 @@
+set -e
+echo "Checking build version number availability for $1 platform"
+set APP_BUILD_VERSION=$(curl "https://api.type.world/latestUnpublishedVersion/world.type.guiapp/$1/?TYPEWORLD_APIKEY=$TYPEWORLD_APIKEY")
+
+if [ "$APP_BUILD_VERSION" = "n/a" ]; then
+    echo "No build version number available: n/a"
+    exit 1
+fi

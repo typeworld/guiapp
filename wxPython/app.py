f1f98a33d1b496d6b8edcd6fd4db4a1c003ad93b
Yanone <post@yanone.de>
2020-11-18 17:32:30 +0100

    Early Windows Hi-DPI support

--- wxPython/app.py
+++ wxPython/app.py
@@ -6678,25 +6678,29 @@ class MyApp(wx.App):
     ):
         try:
 
-            # # Hi-DPI support on Windows, see https://discuss.wxpython.org/t/support-for-high-dpi-on-windows-10/32925/2
-            # try:
-            #     from ctypes import OleDLL
+            # Hi-DPI support on Windows, see https://discuss.wxpython.org/t/support-for-high-dpi-on-windows-10/32925/2
+            try:
+                import ctypes
 
-            #     # Turn on high-DPI awareness to make sure rendering is sharp on big
-            #     # monitors with font scaling enabled.
-            #     OleDLL("shcore").SetProcessDpiAwareness(1)
+                ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
-            # except AttributeError:
-            #     # We're on a non-Windows box.
-            #     pass
+                # from ctypes import OleDLL
 
-            # except OSError:
-            #     # exc.winerror is often E_ACCESSDENIED (-2147024891/0x80070005).
-            #     # This occurs after the first run, when the parameter is reset in the
-            #     # executable's manifest and then subsequent calls raise this exception
-            #     # See last paragraph of Remarks at
-            #     # [https://msdn.microsoft.com/en-us/library/dn302122(v=vs.85).aspx](https://msdn.microsoft.com/en-us/library/dn302122(v=vs.85).aspx)
-            #     pass
+                # # Turn on high-DPI awareness to make sure rendering is sharp on big
+                # # monitors with font scaling enabled.
+                # OleDLL("shcore").SetProcessDpiAwareness(1)
+
+            except AttributeError:
+                # We're on a non-Windows box.
+                pass
+
+            except OSError:
+                # exc.winerror is often E_ACCESSDENIED (-2147024891/0x80070005).
+                # This occurs after the first run, when the parameter is reset in the
+                # executable's manifest and then subsequent calls raise this exception
+                # See last paragraph of Remarks at
+                # [https://msdn.microsoft.com/en-us/library/dn302122(v=vs.85).aspx](https://msdn.microsoft.com/en-us/library/dn302122(v=vs.85).aspx)
+                pass
 
             # Abuse unused "filename" as "startWithCommand"
             self.startWithCommand = filename

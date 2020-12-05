9a25f0d1879c021d8b69955d8a9835a6cefdaef7
Yanone <post@yanone.de>
2020-12-05 10:24:04 +0100

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -22,3 +22,18 @@ cp -R ynlib/Lib/ynlib $SITEPACKAGES
 # Apparently, they're the same file:
 # https://groups.google.com/a/continuum.io/forum/#!topic/anaconda/4HKVl8Jhy9E
 ln -s ~/.localpython3.7.7/lib/libpython3.7m.dylib ~/.localpython3.7.7/lib/libpython3.7.dylib
+
+# Use Python Framework build for testing
+# https://wiki.wxpython.org/wxPythonVirtualenvOnMac
+echo "Python Framework Build Hack"
+
+# what real Python executable to use
+PYVER=3.7
+PYTHON=/Library/Frameworks/Python.framework/Versions/$PYVER/bin/python$PYVER
+
+# find the root of the virtualenv, it should be the parent of the dir this script is in
+ENV=`$PYTHON -c "import os; print(os.path.abspath(os.path.join(os.path.dirname(\"$0\"), '..')))"`
+
+# now run Python with the virtualenv set as Python's HOME
+export PYTHONHOME=$ENV 
+echo $PYTHONHOME

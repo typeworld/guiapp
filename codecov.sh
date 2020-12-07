b069e3958581a45df1536b43fdade1036765929b
Yanone <post@yanone.de>
2020-12-07 14:50:07 +0100

    Add codecov build names

--- codecov.sh
+++ codecov.sh
@@ -1,1 +1,1 @@
-python3 -m coverage run wxPython/app.py selftest && codecov -t 66a9e5cb-f1ad-46c1-9964-8ce0ded70f03
+python3 -m coverage run wxPython/app.py selftest && codecov -n "Mac Locally" -t 66a9e5cb-f1ad-46c1-9964-8ce0ded70f03

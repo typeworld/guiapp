4e96f45090f9219dece7c946606f5a43bcc98979
Yanone <post@yanone.de>
2021-02-01 20:43:48 +0100

    Print original file first

--- wxPython/build/Windows/zmq__init__override.py
+++ wxPython/build/Windows/zmq__init__override.py
@@ -9,7 +9,15 @@ def _delvewheel_init_patch_0_0_9():
     libs_dir = os.path.abspath(
         os.path.join(os.path.dirname(__file__), os.pardir, "pyzmq.libs")
     )
-    os.add_dll_directory(libs_dir)
+    if sys.version_info[:2] >= (3, 8):
+        os.add_dll_directory(libs_dir)
+    else:
+        from ctypes import WinDLL
+
+        with open(os.path.join(libs_dir, ".load_order")) as file:
+            load_order = file.read().split()
+        for lib in load_order:
+            WinDLL(os.path.join(libs_dir, lib))
 
 
 _delvewheel_init_patch_0_0_9()

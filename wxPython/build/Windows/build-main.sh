a7daefd41235b0e8d2e71190794bc991777d4524
Yanone <post@yanone.de>
2021-02-02 12:23:04 +0100

    Update build-main.sh

--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -1,16 +1,18 @@
 set -e
 
-dir C:\\projects\\guiapp\\build\\lib\\
 # ls -la C:\\projects\\guiapp\\build\\lib\\zmq\\
 # ls -la C:\\projects\\guiapp\\build\\lib\\zmq\\.libs\\
 
-# C:\projects\guiapp\build\lib\
+#C:\projects\guiapp\build\lib\
 
 echo "Python build"
 python wxPython/build/Windows/setup.py build > nul # muting too much output
 
 echo "/build content"
 dir build
+
+echo "/build/lib content"
+dir build\\lib
 
 echo "Add Windows App Manifest"
 "$WINDOWSKITBIN\\mt.exe" -manifest "wxPython/build/Windows/windowsAppManifest.xml" -outputresource:build\\TypeWorld.exe;#1

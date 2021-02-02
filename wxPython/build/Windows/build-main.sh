9671e136ce54d831a61919d67bebd5ff847f60c6
Yanone <post@yanone.de>
2021-02-02 11:45:25 +0100

    Update build-main.sh

--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -1,8 +1,8 @@
 set -e
 
-ls -la C:\\projects\\guiapp\\build\\lib\\
-ls -la C:\\projects\\guiapp\\build\\lib\\zmq\\
-ls -la C:\\projects\\guiapp\\build\\lib\\zmq\\.libs\\
+# ls -la C:\\projects\\guiapp\\build\\lib\\
+# ls -la C:\\projects\\guiapp\\build\\lib\\zmq\\
+# ls -la C:\\projects\\guiapp\\build\\lib\\zmq\\.libs\\
 
 echo "Python build"
 python wxPython/build/Windows/setup.py build > nul # muting too much output

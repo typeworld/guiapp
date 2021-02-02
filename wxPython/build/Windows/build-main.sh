a280eabc5f8f236b305f68de5cac5a8a6403f726
Yanone <post@yanone.de>
2021-02-02 10:54:31 +0100

    Update build-main.sh

--- wxPython/build/Windows/build-main.sh
+++ wxPython/build/Windows/build-main.sh
@@ -22,8 +22,8 @@ cp -r ynlib/Lib/ynlib build/lib/
 echo "Copy importlib_metadata"
 cp -r $SITEPACKAGES/importlib_metadata-*.dist-info build/lib/
 
-ls -la C:\\projects\\guiapp\\build\\lib\\
-ls -la C:\\projects\\guiapp\\build\\lib\\pyzmq.libs\\
+# ls -la C:\\projects\\guiapp\\build\\lib\\
+# ls -la C:\\projects\\guiapp\\build\\lib\\pyzmq.libs\\
 
 echo "App Self Test"
 "build/TypeWorld.exe" selftest

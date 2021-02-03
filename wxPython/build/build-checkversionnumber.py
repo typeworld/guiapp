d575a2105bdbb4f01158697ba83c80a909b5247f
Yanone <post@yanone.de>
2021-02-03 17:17:11 +0100

    Update build-checkversionnumber.py

--- wxPython/build/build-checkversionnumber.py
+++ wxPython/build/build-checkversionnumber.py
@@ -5,6 +5,8 @@ import typeworld.api
 version = os.getenv("APP_BUILD_VERSION", "undefined")
 assert version != "undefined"
 
-if typeworld.api.VERSION != version:
-    print(f"typeworld.api doesn’t hold the up-to-date version number {version}")
+if not version.startswith(typeworld.api.VERSION):
+    print(
+        f"typeworld.api doesn’t hold the up-to-date version number {typeworld.api.VERSION}"
+    )
     sys.exit(1)
